8a095e9ded2f50c534b3802ac0ff7b163021e6d0
diego <203414@ids.upchiapas.edu.mx>
2022-03-09 13:10:56 -0600

    Interfaz

---
+++ Interfaz/src/App.js
@@ -0,0 +1,20 @@
+import Login from './Login';
+import './estilos.css';
+import Register from './Register';
+import UserProfile from './UserProfile';
+import {BrowserRouter, Switch, Routes,Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+            <Route path='' element={<Register></Register>}></Route>
+            <Route path='/register' element={<Register></Register>}></Route>
+            <Route path='/login' element={<Login></Login>}/>
+            <Route path='/profile/:user_id'element={<UserProfile></UserProfile>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
